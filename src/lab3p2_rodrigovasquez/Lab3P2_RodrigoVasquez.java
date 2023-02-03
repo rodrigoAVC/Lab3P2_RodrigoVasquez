64b85b80c304b310d6210cd3e49b67c410ee4d1b
Rodrigo Vasquez <rodrigovasquezcabezas@gmail.com>
2023-02-03 16:42:36 -0600

    Se creo un metodo 'agregarVehiculo()' que crea vehiculos y retorna el vehiculo devuelta al main, para alli agregarlo al concesionario y a la listas. Por ahora solo puede agregar bicicletas y motocicletas

--- src/lab3p2_rodrigovasquez/Lab3P2_RodrigoVasquez.java
+++ src/lab3p2_rodrigovasquez/Lab3P2_RodrigoVasquez.java
@@ -34,10 +34,38 @@ public class Lab3P2_RodrigoVasquez {
                     crudCliente(clientes);
                     break;
                 case 3:
+                    int opcionVehiculo;
                     if (concesionarias.size() == 0) {
                         System.out.println("No se pueden agregar vehículos si no hay concesionarias");
                     } else {
-
+                        do {
+                            System.out.println("-----------------------------------");
+                            System.out.println("       < V E H I C U L O S >");
+                            System.out.println("-----------------------------------");
+                            System.out.print("""
+                                    [1] Agregar
+                                    [2] Modificar
+                                    [3] Eliminar 
+                                    [4] Salir
+                               """);
+                            System.out.println("-----------------------------------");
+                            System.out.print("Ingrese la accion a realizar: ");
+                            opcionVehiculo = scMain.nextInt();
+                            switch (opcionVehiculo) {
+                                case 1:
+                                    agregarVehiculo();
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    System.out.println("Saliendo...");
+                                    break;
+                                default:
+                                    System.out.println("Opcion Invalida");
+                            }
+                        } while (opcionVehiculo != 4);
                     }
                     break;
                 case 4:
@@ -108,6 +136,98 @@ public class Lab3P2_RodrigoVasquez {
         return clientes;
     }
 
+    public static Vehiculo agregarVehiculo() {
+        Scanner scVehi = new Scanner(System.in);
+        System.out.print("Ingrese cuantas llantas tiene el vehiculo [2 / 4]: ");
+        int llantas = scVehi.nextInt();
+        if (llantas == 2) {
+            System.out.println("---------------");
+            System.out.print("""
+                               [1] Motocicleta
+                               [2] Bicicleta 
+                               """);
+            System.out.println("---------------");
+            System.out.print("¿Que desea añadir? ");
+            int tipoInt = scVehi.nextInt();
+            if (tipoInt == 1) {
+                System.out.print("Ingrese el color de la motocicleta: ");
+                String colorMoto = scVehi.nextLine();
+                colorMoto = scVehi.nextLine();
+                System.out.print("Ingrese la marca de la motocicleta: ");
+                String marcaMoto = scVehi.nextLine();
+                System.out.print("Ingrese el modelo de la motocicleta: ");
+                String modeloMoto = scVehi.nextLine();
+                System.out.print("Ingrese el año de fabricación: ");
+                int yearMoto = scVehi.nextInt();
+                System.out.print("Ingrese el precio del vehículo: ");
+                int precioMoto = scVehi.nextInt();
+                int cantLlantas = 2;
+                System.out.print("Ingrese el tipo de desplazamiento que tiene la motocicleta: ");
+                String desplazamiento = scVehi.nextLine();
+                desplazamiento = scVehi.nextLine();
+                char caracter;
+                boolean electrica = false;
+                do {
+                    System.out.print("¿Es la moto electrica? [S/N]: ");
+                    caracter = scVehi.next().charAt(0);
+                    if (caracter == 's' || caracter == 'S' || caracter == 'n' || caracter == 'N') {
+                        if (caracter == 's' || caracter == 'S') {
+                            electrica = true;
+                        } else {
+                            electrica = false;
+                        }
+                    } else {
+                        System.out.println("Opcion Invalida");
+                    }
+                } while (caracter != 's' && caracter != 'S' && caracter != 'n' && caracter != 'N');
+                Motocicleta moto = new Motocicleta(desplazamiento, electrica, colorMoto, marcaMoto, modeloMoto, yearMoto, precioMoto, cantLlantas);
+                return moto;
+            } else if (tipoInt == 2) {
+                System.out.print("Ingrese el color de la bicicleta: ");
+                String colorBici = scVehi.nextLine();
+                colorBici = scVehi.nextLine();
+                System.out.print("Ingrese la marca de la bicicleta: ");
+                String marcaBici = scVehi.nextLine();
+                System.out.print("Ingrese el modelo de la bicicleta: ");
+                String modeloBici = scVehi.nextLine();
+                System.out.print("Ingrese el año de fabricación: ");
+                int yearBici = scVehi.nextInt();
+                System.out.print("Ingrese el precio del vehículo: ");
+                int precioBici = scVehi.nextInt();
+                int cantLlantas = 2;
+                System.out.print("Ingrese el radio de la rueda ");
+                double radio = scVehi.nextDouble();
+                System.out.print("Ingrese una descripcion de la bicicleta: ");
+                String descripcion = scVehi.nextLine();
+                descripcion = scVehi.nextLine();
+                String tipo = "";
+                int tipoBici;
+                do {
+                    System.out.println("""
+                                   [1] BMX
+                                   [2] Calle
+                                   """);
+                    System.out.print("Ingrese el tipo de bicicleta: ");
+                    tipoBici = scVehi.nextInt();
+                    if (tipoBici == 1) {
+                        tipo = "BMX";
+                    } else if (tipoBici == 2) {
+                        tipo = "Calle";
+                    } else {
+                        System.out.println("Opcion Invalida");
+                    }
+                } while (tipoBici != 1 && tipoBici != 2);
+                Bicicleta bici = new Bicicleta(descripcion, tipo, radio, colorBici, marcaBici, modeloBici, yearBici, precioBici, cantLlantas);
+                return bici;
+            } else {
+                System.out.println("Opcion Invalida");
+            }
+        } else if (llantas == 4) {
+            
+        } else {
+            System.out.println("Opcion Invalida");
+        }
 
+    }
 
 }
