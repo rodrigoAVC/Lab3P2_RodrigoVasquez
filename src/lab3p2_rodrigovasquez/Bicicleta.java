8a3915379c50abc19320fd4571b0243b58bf7b83
Rodrigo Vasquez <rodrigovasquezcabezas@gmail.com>
2023-02-03 14:29:49 -0600

    Se crearon todas las clases y clases hijas (elementos añadidos a través del insertCode y no validadas aún) y se creo una base para el menu)

---
+++ src/lab3p2_rodrigovasquez/Bicicleta.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3p2_rodrigovasquez;
+
+/**
+ *
+ * @author Rui
+ */
+public class Bicicleta {
+    
+}
--- src/lab3p2_rodrigovasquez/Lab3P2_RodrigoVasquez.java
+++ src/lab3p2_rodrigovasquez/Lab3P2_RodrigoVasquez.java
@@ -1,20 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab3p2_rodrigovasquez;
 
-/**
- *
- * @author Rui
- */
+import java.util.Scanner;
+
 public class Lab3P2_RodrigoVasquez {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //ArrayList & Clase = Consecionaria & Clientes 
+        //Clase Madre = Vehiculo 
+        //Clase Hijos = Carro, Camion, Bus, Motocicleta, Bicecleta 
+        Scanner scMain = new Scanner(System.in);
+        int opcion;
+        do{
+            System.out.println("---------------------------------------------------------");
+            System.out.println("                 < B I E N V E N I D O >                 ");
+            System.out.println("---------------------------------------------------------");
+            System.out.print("""
+                               [1] CRUD Concesionaria 
+                               [2] CRUD Clientes
+                               [3] CRUD Vehículos
+                               [4] Compra / Venta de Vehículos por parte de un Cliente
+                               [5] Salida
+                               """);
+            System.out.println("---------------------------------------------------------");
+            System.out.print("Ingrese que accion desea realizar: ");
+            opcion = scMain.nextInt();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("Cerrando el sistema...");
+                    System.out.println("Muchas gracias por utilizar este programa");
+                    break;
+                default:
+                    System.out.println("Opcion Invalida");
+                    break;
+            }
+        } while (opcion != 5); 
     }
     
 }
